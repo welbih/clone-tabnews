aab7f6a0775dcc97817896973def00dfd1333c43
Welber Marques <welbih@yahoo.com.br>
2023-08-22 18:54:25 +0000

    Definindo mensagem de construção

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-  return (
-    <h1>Olá, dona Paty, dê um sorrisinho para o sniper, será uma boa ideia.</h1>
-  );
+  return <h1>Olá, no momento estamos na fase de construção...</h1>;
 }
 
 export default Home;
